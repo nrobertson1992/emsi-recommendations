50ca12e0296bd2693d778bae4d6b5adb568a23b0
nrobertson1992 <nrobertson@edx.org>
2022-12-16 09:14:38 -0700

    Update snowflake_queries.py

--- snowflake_queries.py
+++ snowflake_queries.py
@@ -47,52 +47,14 @@ def get_snowflake_data(query, columns):
 
 # Get courses to match with a company / industry.
 
-sql1 = """
-SELECT
-    tcs.course_key,
-    split_part(tcs.course_key,'+',1) as partner,
-    cmc.title,
-    cmc.short_description,
-    'https://www.edx.org/course/' || cmc.url_slug as url,
-    'https://prod-discovery.edx-cdn.org/' || cmc.image as image_link,
-    cmc.enrollment_count,
-    LISTAGG(ts.name, ', ') as skills
-FROM
-    discovery_pii.taxonomy_courseskills as tcs
-LEFT JOIN
-    discovery_pii.taxonomy_skill as ts
-ON
-    tcs.skill_id = ts.id
-LEFT JOIN
-    discovery_pii.course_metadata_course as cmc
-ON
-    tcs.course_key = cmc.key
-WHERE
-    tcs.is_blacklisted = FALSE
-AND
-    cmc.enrollment_count >= 100
-AND
-    cmc.draft = False
-GROUP BY
-    1,2,3,4,5,6,7
-ORDER BY
-    7 DESC
-        """
+sql1 = st.secrets['info']['sql1']
 
 cols1 = ['course_key','partner','title','short_description','url','image_link','enrollment_count', 'skills']
 
 
 # Get business subscription catalog.
 
-sql2 = """
-SELECT
-    qtc.course_key
-FROM
-    enterprise.map_catalog_query_to_content qtc
-WHERE
-    -- UUID for B2B Subscription catalog.
-    catalog_query_uuid = 'b90a0c2339ca47acb087a31c4586c08b'
-"""
+sql2 = st.secrets['info']['sql2']
 
 cols2 = ['course_key']
 
@@ -101,15 +63,7 @@ b2b_subs_catalog = get_snowflake_data(query=sql2, columns=cols2)
 
 # Get online campus subscription catalog.
 
-sql3 = """
-SELECT
-    qtc.course_key
-FROM
-    enterprise.map_catalog_query_to_content qtc
-WHERE
-    -- UUID for OC Subscription catalog.
-    catalog_query_uuid = '703cfd8977b44aa38f733a9915be3897'
-"""
+sql3 = st.secrets['info']['sql3']
 
 cols3 = ['course_key']
 
@@ -119,26 +73,7 @@ oc_subs_catalog = get_snowflake_data(query=sql3, columns=cols3)
 
 # Get job-skill mappings, ranked by # of unique postings.
 
-sql4 = """
-SELECT
-    tj.name as job_name,
-    tjs.significance,
-    tjs.unique_postings,
-    ts.name as skill_name
-FROM 
-    discovery_pii.taxonomy_jobskills as tjs
-LEFT JOIN
-    discovery_pii.taxonomy_job as tj
-ON
-    tjs.job_id = tj.id
-LEFT JOIN
-    discovery_pii.taxonomy_skill as ts
-ON
-    tjs.skill_id = ts.id
-ORDER BY
-    tj.name,
-    tjs.unique_postings DESC
-"""
+sql4 = st.secrets['info']['sql4']
 
 cols4 = ['job_name','signficance','unique_postings','skill_name']
 
